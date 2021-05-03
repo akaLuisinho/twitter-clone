11765db0f8c806a6dfbd0d6ae8d00dab42881800
Luis Felipe <luis.felipeszsv@gmail.com>
2021-05-03 19:48:48 -0300

    developing main and bottommenu

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { 
+    Container, 
+    Header, 
+    BackIcon,
+    ProfileInfo, 
+    BottomMenu,
+    HomeIcon,
+    SearchIcon,
+    BellIcon,
+    EmailIcon
+} from './styles';
+
+function Main() {
+  return (
+      <Container>
+          <Header>
+              <button>
+                  <BackIcon/>
+              </button>
+
+              <ProfileInfo>
+                  <strong>Guilherme Rodz</strong>
+                  <span>613 Tweets</span>
+              </ProfileInfo>
+          </Header>
+            {/* <ProfilePage/> */}
+
+        <BottomMenu>
+            <HomeIcon/>
+            <SearchIcon/>
+            <BellIcon/>
+            <EmailIcon/>
+        </BottomMenu>
+      </Container>
+  );
+}
+
+export default Main;
